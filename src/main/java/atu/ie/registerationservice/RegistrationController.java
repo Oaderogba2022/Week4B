e8f49f75860485622bb95fa75cfb39f1638a11c6
Olamide Aderogba <G00401971@atu.ie>
2023-10-23 11:22:32 +0100

    Changed Port in the application properties and added code to the RegistrationController and RegistrationServiceApplication to return a confirmation message to the new user.

--- src/main/java/atu/ie/registerationservice/RegistrationController.java
+++ src/main/java/atu/ie/registerationservice/RegistrationController.java
@@ -1,5 +1,17 @@
 package atu.ie.registerationservice;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class RegistrationController {
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody UserDetails userDetails) {
+        String confirmationMessage = String.format("Received details for %s with email %s", userDetails.getName(),userDetails.getEmail());
+        return confirmationMessage;
+    }
+
+
 
 }
--- src/main/java/atu/ie/registerationservice/RegistrationServiceApplication.java
+++ src/main/java/atu/ie/registerationservice/RegistrationServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegisterationServiceApplication {
+public class RegistrationServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RegisterationServiceApplication.class, args);
+        SpringApplication.run(RegistrationServiceApplication.class, args);
     }
 
 }
